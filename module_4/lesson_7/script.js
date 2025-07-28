ae52df4ac3263f86b4a26331261867f143eadc5c
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2025-07-28 21:59:02 +0300

    lesson_7

---
+++ module_4/lesson_7/script.js
@@ -0,0 +1,61 @@
+// -------------------------------- 5
+
+/**
+ * Напиши скрипт, який для об'єкта user, послідовно:
+ * - додає поле mood зі значенням 'happy'
+ * - замінює значення hobby на 'skydiving'
+ * - замінює значення premium на false
+ * - виводить вміст об'єкта user у форматі ключ:значення використовуючи Object.keys() та for...of
+ */
+
+// const user = {
+//   name: "Alice",
+//   age: 20,
+//   hobby: "html",
+//   premium: true,
+// };
+
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const keys = Object.keys(user);
+
+// for(const key of keys) {
+//     console.log(`${key}: ${user[key]}`);
+// }
+
+
+
+
+
+
+
+
+
+/**
+ * У нас є об'єкт, де зберігаються зарплати нашої команди. Напишіть код для
+ * підсумовування всіх зарплат і збережіть результат у змінній sum.
+ * Повинно вийти 390. Якщо об'єкт salaries порожній, то результат має бути 0.
+ */
+
+// const salaries = {
+//     alice: 160,
+//     yura: 130,
+//     petya: 100
+// }
+
+// function summa(obj) {
+//     const values = Object.values(obj);
+//     let sum = 0;
+
+//     for(const item of values) {
+//         sum += item; 
+//     }
+//     return sum;
+// }
+
+
+// console.log(summa(salaries));
+
