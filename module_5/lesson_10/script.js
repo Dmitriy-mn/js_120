06816d6711bb7afcc24068e1181a2f4d188d6041
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2025-08-06 22:18:23 +0300

    lesson_10

---
+++ module_5/lesson_10/script.js
@@ -0,0 +1,273 @@
+
+/**
+ * Сортування масиву об'єктів
+ */
+// const players = [
+//   { id: "player-1", name: "Mango", timePlayed: 310, online: false },
+//   { id: "player-2", name: "Poly", timePlayed: 470, online: true },
+//   { id: "player-3", name: "Aiwi", timePlayed: 230, online: true },
+//   { id: "player-4", name: "Ajax", timePlayed: 150, online: false },
+//   { id: "player-5", name: "Chelsey", timePlayed: 80, online: true },
+// ];
+
+
+// // За ігровим часом
+
+
+// const result = players.toSorted((a, b) => a.timePlayed - b.timePlayed);
+// const result = players.toSorted((a, b) => b.timePlayed - a.timePlayed);
+
+// console.table(result);
+
+
+
+
+/**
+ * -------------------------------------
+ */
+// const allCars = [
+//   { make: "Honda", model: "CR-V", type: "suv", amount: 14, price: 24045, onSale: true},
+//   { make: "Honda", model: "Accord", type: "sedan", amount: 2, price: 22455, onSale: true },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", amount: 8, price: 24195, onSale: false },
+//   { make: "Mazda", model: "CX-9", type: "suv", amount: 7, price: 31520, onSale: true },
+//   { make: "Toyota", model: "4Runner", type: "suv", amount: 19, price: 34210, onSale: false },
+//   { make: "Toyota", model: "Sequoia", type: "suv", amount: 16, price: 45560, onSale: false },
+//   { make: "Toyota", model: "Tacoma", type: "truck", amount: 4, price: 24320, onSale: true },
+//   { make: "Ford", model: "F-150", type: "truck", amount: 11, price: 27110, onSale: true },
+//   { make: "Ford", model: "Fusion", type: "sedan", amount: 13, price: 22120, onSale: true },
+//   { make: "Ford", model: "Explorer", type: "suv", amount: 6, price: 31660, onSale: false }
+// ];
+
+
+/**
+ * Нехай функція getModelsOnSale повертає масив моделей автомобілів,
+ * але тільки тих, які зараз на розпродажі.
+ */
+
+// const getModelsOnSale = (cars) => {
+//     return cars
+//         .filter((car) => car.onSale)
+//         .map((car) => car.model)
+// }
+
+// console.log(getModelsOnSale(allCars));
+
+
+
+
+
+
+
+/**
+ * Нехай функція getSortedCarsOnSale повертає масив автомобілів
+ * на розпродажі (Властивість onSale), відсортованих за зростанням ціни.
+ */
+
+
+// const getSortedCarsOnSale = (cars) => {
+//     return cars
+//         .filter((car) => car.onSale)
+//         .toSorted((a, b) => a.price - b.price)
+// }
+
+// console.log(getSortedCarsOnSale(allCars));
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// Необхідно відфільтрувати студентів, які мають:
+
+// високі бали (від 80 (включно)),
+// низькі бали (нижче 50),
+// середні бали (від 50 (включно) до 80).
+
+
+// const LOW_SCORE = 50;
+// const HIGH_SCORE = 80;
+// const students = [
+//   { name: "Mango", score: 83 },
+//   { name: "Poly", score: 59 },
+//   { name: "Ajax", score: 37 },
+//   { name: "Kiwi", score: 94 },
+//   { name: "Houston", score: 64 },
+// ];
+
+// const best = students.filter(student => student.score >= HIGH_SCORE);
+// console.log(best); // Масив об'єктів з іменами Mango і Kiwi
+
+// const worst = students.filter(student => student.score < LOW_SCORE);
+// console.log(worst); // Масив з одним об'єктом Ajax
+
+// const average = students.filter(
+//   (student) => student.score >= LOW_SCORE && student.score < HIGH_SCORE
+// );
+// console.log(average); // Масив об'єктів з іменами Poly і Houston
+
+
+
+
+
+// --------------------------------------- 2
+
+/**
+ * Метод find
+ */
+
+
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
+
+// colorPickerOptions.find((option) => option.label === "blue"); // { label: "blue", color: "#2196F3" }
+// colorPickerOptions.find((option) => option.label === "pink"); // { label: "pink", color: "#E91E63" }
+// colorPickerOptions.find((option) => option.label === "white"); // undefined
+
+
+
+
+
+
+
+// -------------------------------------- 3
+
+
+
+
+// every, some
+
+
+// чи всі товари є в наявності
+
+// const products = [
+// 	{ name: "apple", quantity: 2 },
+// 	{ name: "orange", quantity: 5 },
+// 	{ name: "plum", quantity: 0 },
+// ];
+
+// const hasEveryProduct = products.every(product => product.quantity > 0);
+// console.log(hasEveryProduct); // false
+
+
+
+
+
+
+/**
+ * Метод some
+ *
+ */
+
+// Чи є хоча б один елемент, що більший або дорівнює нулю? - так
+// [1, 2, 3, 4, 5].some(value => value >= 0); // true
+
+// Чи є хоча б один елемент, що більший або дорівнює нулю? - так
+// [-7, -20, 3, -10, -14].some(value => value >= 0); // true
+
+// Чи є хоча б один елемент, що менший від нуля? - ні
+// [1, 2, 3, 4, 5].some(value => value < 0); // false
+
+// Чи є хоча б один елемент, що менший від нуля? - так
+// [1, 2, 3, -10, 4, 5].some(value => value < 0); // true
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------- 4
+
+/**
+ * Метод reduce
+ *
+ * - Поелементно перебирає оригінальний масив
+ * - Значення, що повертається, залежить від розробника
+ */
+
+
+// const numbers = [2, 7, 3]
+
+// const total = numbers.reduce((previousValue, number) => {
+//   return previousValue + number;
+// }, 0);
+
+
+
+
+//  у нас є масив студентів з балами за тест. Необхідно отримати середній бал.
+                           
+
+// const students = [
+//   { name: "Mango", score: 83 },
+//   { name: "Poly", score: 59 },
+//   { name: "Ajax", score: 37 },
+//   { name: "Kiwi", score: 94 },
+//   { name: "Houston", score: 64 },
+// ];
+
+// // Назва акумулятора може бути довільною, це просто параметр функції
+// const totalScore = students.reduce((total, student) => {
+//   return total + student.score;
+// }, 0);
+
+// const averageScore = totalScore / students.length;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------- 5
+
+/**
+ * Метод toSorted
+ *
+ */
+
+
+// const students = [
+//   { name: "Mango", score: 83 },
+//   { name: "Poly", score: 59 },
+//   { name: "Ajax", score: 37 },
+//   { name: "Kiwi", score: 94 },
+// ];
+
+// const inAscendingScoreOrder = students.toSorted(
+//   (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+// );
+
+// const inDescendingScoreOrder = students.toSorted(
+//   (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+// );
+
+// const inAlphabeticalOrder = students.toSorted((firstStudent, secondStudent) =>
+//   firstStudent.name.localeCompare(secondStudent.name)
+// );
+
+
+
+
+
+
+
+
+
+
+
