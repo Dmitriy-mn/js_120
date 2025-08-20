f888952b8fe93fe63fca59272e979677da238758
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2025-08-20 22:20:45 +0300

    lesson_14

---
+++ module_7/lesson_14/js/01-events.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".my-button");
+
+button.addEventListener("click", () => {
+  console.log("The button was pressed and now the next image will appear");
+});
+
+
+
+
+
